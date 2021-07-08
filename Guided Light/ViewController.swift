029de498500d34db8dc47ebccef53718e9c0e8c2
mmyau1 <86981889+mmyau1@users.noreply.github.com>
2021-07-08 11:12:26 -0500

    Flashlight Lab/App
    Button that changes background color

--- Guided Light/ViewController.swift
+++ Guided Light/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var lightOn = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
+    
 }
 
